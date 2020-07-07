2891481c1aac32885f41e04680d5127de89494b8
Bassit <barseetbrown@gmail.com>
2020-07-07 20:11:55 +0100

    wired profile page

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import '../../styles/profile.css';
 
 const Profile = () => {
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profile-container">
+      <div className="profile-top">
+        <div className="profile-photo">
+          <img
+            src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+        </div>
+        <div>
+          <h4>Breellz Brown</h4>
+          <div className="profile-data">
+            <h5>50 posts</h5>
+            <h5>50 followers</h5>
+            <h5>50 following</h5>
+          </div>
+        </div>
+      </div>
+      <div className="gallery">
+        <img
+          className="gallery-item"
+          src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <img
+          className="gallery-item"
+          src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <img
+          className="gallery-item"
+          src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <img
+          className="gallery-item"
+          src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <img
+          className="gallery-item"
+          src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <img
+          className="gallery-item"
+          src="https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
