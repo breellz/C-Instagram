da1918e6f23f77612d1b30bb2f4364e1115a8ef9
Bassit <barseetbrown@gmail.com>
2020-07-07 20:44:56 +0100

    wired Home page

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import '../../styles/Home.css';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home-wrapper">
+      <div className="card home-card">
+        <h5>Breellz</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1569124589354-615739ae007b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+        </div>
+        <div className="card-content">
+          <i className="material-icons">favorite</i>
+          <h6>Post title</h6>
+          <p>Post body goes here</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Breellz</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1569124589354-615739ae007b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+        </div>
+        <div className="card-content">
+          <i className="material-icons">favorite</i>
+          <h6>Post title</h6>
+          <p>Post body goes here</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Breellz</h5>
+        <div className="card-image">
+          <img
+            src="https://images.unsplash.com/photo-1569124589354-615739ae007b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt=""
+          />
+        </div>
+        <div className="card-content">
+          <i className="material-icons">favorite</i>
+          <h6>Post title</h6>
+          <p>Post body goes here</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
     </div>
   );
 };
