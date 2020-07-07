0c9ee1f4e9deb8371a4b9df27a3ea82647b758bf
Bassit <barseetbrown@gmail.com>
2020-07-07 18:06:41 +0100

    added posts api

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/screens/Home';
+import Login from './components/screens/Login';
+import Profile from './components/screens/Profile';
+import SignUp from './components/screens/SignUp';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Route path="/" component={Home} exact />
+      <Route path="/login" component={Login} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/signup" component={SignUp} />
+    </BrowserRouter>
+
+  );
+};
 
 export default App;
