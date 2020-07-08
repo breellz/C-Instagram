e203cf926642c90e3f66ed4c1db111d3c5fd6284
Bassit <barseetbrown@gmail.com>
2020-07-08 19:59:36 +0100

    added public and private routes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import UserContext from '../context/UserContext';
 
 const Navbar = () => {
+  const { state, dispatch } = useContext(UserContext);
+  const Links = () => {
+    if (state) {
+      return [
+        <li><Link to="/profile">Profile</Link></li>,
+        <li><Link to="/create">Create Post</Link></li>
+      ];
+    }
+    return [
+      <li><Link to="/signin">Sign In</Link></li>,
+      <li><Link to="/signup">Sign Up</Link></li>
+    ];
+  };
+
   return (
     <nav>
       <div className="nav-wrapper white">
-        <Link to="/" className="brand-logo left">C-Instagram</Link>
+        <Link to={ state ? '/' : '/signin'} className="brand-logo left">C-Instagram</Link>
         <ul id="nav-mobile" className="right">
-          <li><Link to="/signin">Sign In</Link></li>
-          <li><Link to="/signup">Sign Up</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/create">Create Post</Link></li>
+          {Links()}
         </ul>
       </div>
     </nav>
